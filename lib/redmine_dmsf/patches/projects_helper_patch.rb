a1f3025e3aa2e79ee0ecdf155936dec787ebb4b1
sim <smolinsim@gmail.com>
2020-01-29 19:24:04 +0400

    Для совместимости projects_helper

--- lib/redmine_dmsf/patches/projects_helper_patch.rb
+++ lib/redmine_dmsf/patches/projects_helper_patch.rb
@@ -22,16 +22,32 @@ module RedmineDmsf
   module Patches
     module ProjectHelperPatch
 
+      def project_settings_tabs_without_patch
+        tabs = [{:name => 'info', :action => :edit_project, :partial => 'projects/edit', :label => :label_information_plural},
+                {:name => 'modules', :action => :select_project_modules, :partial => 'projects/settings/modules', :label => :label_module_plural},
+                {:name => 'members', :action => :manage_members, :partial => 'projects/settings/members', :label => :label_member_plural},
+                {:name => 'versions', :action => :manage_versions, :partial => 'projects/settings/versions', :label => :label_version_plural,
+                 :url => {:tab => 'versions', :version_status => params[:version_status], :version_name => params[:version_name]}},
+                {:name => 'categories', :action => :manage_categories, :partial => 'projects/settings/issue_categories', :label => :label_issue_category_plural},
+                {:name => 'wiki', :action => :manage_wiki, :partial => 'projects/settings/wiki', :label => :label_wiki},
+                {:name => 'repositories', :action => :manage_repository, :partial => 'projects/settings/repositories', :label => :label_repository_plural},
+                {:name => 'boards', :action => :manage_boards, :partial => 'projects/settings/boards', :label => :label_board_plural},
+                {:name => 'activities', :action => :manage_project_activities, :partial => 'projects/settings/activities', :label => :enumeration_activities}
+        ]
+        tabs.select {|tab| User.current.allowed_to?(tab[:action], @project)}
+      end
+
+
       ##################################################################################################################
       # Overridden methods
 
       def project_settings_tabs
-        tabs = super
+        tabs = project_settings_tabs_without_patch
         dmsf_tabs = [
-          {:name => 'dmsf', :action => {:controller => 'dmsf_state', :action => 'user_pref_save'},
-           :partial => 'dmsf_state/user_pref', :label => :menu_dmsf},
-          {:name => 'dmsf_workflow', :action => {:controller => 'dmsf_workflows', :action => 'index'},
-           :partial => 'dmsf_workflows/main', :label => :label_dmsf_workflow_plural}
+            {:name => 'dmsf', :action => {:controller => 'dmsf_state', :action => 'user_pref_save'},
+             :partial => 'dmsf_state/user_pref', :label => :menu_dmsf},
+            {:name => 'dmsf_workflow', :action => {:controller => 'dmsf_workflows', :action => 'index'},
+             :partial => 'dmsf_workflows/main', :label => :label_dmsf_workflow_plural}
         ]
         tabs.concat(dmsf_tabs.select {|dmsf_tab| User.current.allowed_to?(dmsf_tab[:action], @project)})
         tabs
@@ -43,8 +59,8 @@ end
 
 if Redmine::Plugin.installed?(:easy_extensions)
   RedmineExtensions::PatchManager.register_helper_patch 'ProjectsHelper',
-    'RedmineDmsf::Patches::ProjectHelperPatch', prepend: true
+                                                        'RedmineDmsf::Patches::ProjectHelperPatch', prepend: true
 else
   RedmineExtensions::PatchManager.register_patch_to_be_first 'ProjectsHelper',
-  'RedmineDmsf::Patches::ProjectHelperPatch', prepend: true, first: true
+                                                             'RedmineDmsf::Patches::ProjectHelperPatch', prepend: true, first: true
 end
